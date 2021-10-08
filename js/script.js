15cabde57b0b390b90f0ace8c325720151001eb9
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-08 17:48:31 +0200

    Corretto il codice e aggiunte le cifre decimali

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ const prezzoParziale = kilometri * 0.21
 
 if (eta >= 18 && eta < 65) {
     let prezzoTotale = prezzoParziale
-    alert("Il prezzo del suo viaggio è: " + prezzoTotale)
+    alert("Il prezzo del suo viaggio è: " + prezzoTotale + "€")
 } else if (eta < 18) {
-    let prezzoTotale = prezzoParziale * 20 / 100
-    alert("Il prezzo del suo viaggio è: " + prezzoTotale)
+    let scontoMinori = prezzoParziale * 20 / 100
+    let prezzoTotale = prezzoParziale - scontoMinori
+    let pT = prezzoTotale.toFixed(2);
+    alert("Il prezzo del suo viaggio è: " + pT + "€")
 } else if (eta > 65) {
-    let prezzoTotale = prezzoParziale * 40 / 100
-    alert("Il prezzo del suo viaggio è: " + prezzoTotale)
+    let scontoVecchi = prezzoParziale * 40 / 100
+    let prezzoTotale = prezzoParziale - scontoVecchi
+    let pT = prezzoTotale.toFixed(2);
+    alert("Il prezzo del suo viaggio è: " + pT + "€")
 } else {
     alert("Nessun risultato disponibile!")
 }
